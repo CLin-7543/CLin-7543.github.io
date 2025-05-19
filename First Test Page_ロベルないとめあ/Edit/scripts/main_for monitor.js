f8341c1ca648ca2805177846b820026fe32b13dd
CLin-7543 <ab2689724@gmail.com>
2025-05-19 16:50:45 +0800

    Update main_for monitor.js

--- First Test Page_ロベルないとめあ/Edit/scripts/main_for monitor.js
+++ First Test Page_ロベルないとめあ/Edit/scripts/main_for monitor.js
@@ -27,7 +27,7 @@ function setUserName() {
       setUserName();
     } else {
       localStorage.setItem("name", myName);
-      myHeading.textContent = `Mozilla is cool, ${myName}`;
+      myHeading.textContent = `ロベルないとめあはおもろいね！, ${myName}`;
     }
 }
   
